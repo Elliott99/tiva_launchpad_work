eee82447af039f88162412703211781c806290c9
Elliott99 <43041725+Elliott99@users.noreply.github.com>
2021-09-15 13:43:23 -0400

    Update main.s
    A simple assembly program that enables the clock gate for Port F, sets Pin F1 as output and digitally enables it to send voltage into the GPIO port, and then load bit 1 into the data register for Pin F1, turning on the red LED

--- main.s
+++ main.s
@@ -3,7 +3,6 @@ GPIO_PORTF_DIR_R EQU 0x40025400
 GPIO_PORTF_DEN_R EQU 0x4002551C
 GPIO_PORTF_DATA_R EQU 0x400253FC
 	
-SEC_DIV_FIVE EQU 200
 
 	AREA	|.text|,CODE,READONLY,ALIGN=2	
 				THUMB
@@ -13,20 +12,9 @@ Main
 
 loop
 	LDR R1, =GPIO_PORTF_DATA_R
-	MOV R0, #0xE
+	MOV R0, #0x02 ;;mov bit 1 into the Data register for GPIO Portf
 	STR R0, [R1]
-	BL delay
-	LDR R1, =GPIO_PORTF_DATA_R
-	MOV R0, #0x08
-	STR R0, [R1]
-	BL delay
 	B loop
-
-delay 
-	LDR R1, =SEC_DIV_FIVE
-	SUBS R1, R1, #1
-	BNE delay
-	BX LR
 
 
 GPIOF_init
@@ -35,15 +23,13 @@ GPIOF_init
 	ORR R0,R0,#0x20; set 5th bit of register (set clock for PORTF)
 	STR R0, [R1]
 	LDR R1, =GPIO_PORTF_DIR_R
-	MOV R0, #0xE
+	MOV R0, #0x02 ;set pin F1 as output, which controls the red LED
 	STR R0, [R1]
 	LDR R1, =GPIO_PORTF_DEN_R
-	MOV R0, #0xE
-	STR R0, [R1]
+	MOV R0, #0x02
+	STR R0, [R1] ;digitally enable Pin F2 to send voltage to the red LED
 	BX LR
 
 	
 	ALIGN
 	END
-	
-	
